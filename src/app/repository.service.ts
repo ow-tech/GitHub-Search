4a2d23ad568b483d0a5b1a4e17298e302dc4ab83
ow-tech <wagwanwekon@outloo.com>
2020-04-10 17:24:35 +0300

    creating a function to get users repos

--- src/app/repository.service.ts
+++ src/app/repository.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepositoryService {
+  private username:string;
+  private clientid:'Iv1.394bac6b6094525f';
+  private clientsecret:'901a5bfed068ccd1b2d841158e719b308d8f2214';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getUserRepo(){
+    return this.http.get("https://api.github.com/users/" + this.username+ "/repos?client_id="+ this.clientid + "&client_secret=" + this.clientsecret);
+  }
 }
