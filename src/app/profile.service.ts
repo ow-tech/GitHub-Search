570dc1740106d2e17bac3ad7cd4afa43bf7816f9
ow-tech <wagwanwekon@outloo.com>
2020-04-10 14:03:33 +0300

    imports of httpmoule to profile service

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import {Http, Headers} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  
+  constructor(private http:Http) { }
 }
