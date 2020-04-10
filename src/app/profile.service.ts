b399a362a25eee53128716adc1b42054b447cbd1
ow-tech <wagwanwekon@outloo.com>
2020-04-10 14:10:36 +0300

    creating of properties to be used for the profile service

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -7,6 +7,9 @@ import 'rxjs/add/operator/map';
 })
 export class ProfileService {
 
+  private username:string;
+  private clientid-'Iv1.394bac6b6094525f';
+  private clientsecret:'901a5bfed068ccd1b2d841158e719b308d8f2214';
   
   constructor(private http:Http) { }
 }
