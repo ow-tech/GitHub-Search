1cb11b1a64ed9893757ecff13e90c8159eb5d038
ow-tech <wagwanwekon@outloo.com>
2020-04-10 14:16:08 +0300

    definig of username

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Http, Headers} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 @Injectable({
@@ -8,8 +8,13 @@ import 'rxjs/add/operator/map';
 export class ProfileService {
 
   private username:string;
-  private clientid-'Iv1.394bac6b6094525f';
+  private clientid:'Iv1.394bac6b6094525f';
   private clientsecret:'901a5bfed068ccd1b2d841158e719b308d8f2214';
   
-  constructor(private http:Http) { }
+  constructor(private http:HttpClient) { 
+    // to check if the service is runnig
+    console.log ("service perfecly working")
+    // defining the user name.(everytime, landing page will display my profile)
+    this.username= "ow-tech";
+  }
 }
