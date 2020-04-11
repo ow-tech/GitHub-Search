8ba6264467ff71fbee9d090e63bf5e6b6370bba0
ow-tech <wagwanwekon@outloo.com>
2020-04-11 17:32:15 +0300

    re-writing of duration pipe logic

--- src/app/duration.pipe.ts
+++ src/app/duration.pipe.ts
@@ -7,20 +7,48 @@ export class DurationPipe implements PipeTransform {
 
   transform(value: any,): any {
     
-    let currentDate:any=new Date();
-    let today:any=new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate())
-    let dateDifference = Math.abs(today- value) //returns value in miliseconds
-    const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
-    let milisecondsToSeconds= dateDifference*0.001; //converts miliseconds to seconds
-    let duration = milisecondsToSeconds/secondsInDay;
-    let less:string='less than a year ago'
-    if (duration>=1){
-      return duration;
-    }else{
-      return less;
+//     let currentDate:any=new Date();
+//     let today:any=new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate())
+//     let dateDifference = Math.abs(today- value) //returns value in miliseconds
+//     const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
+//     let milisecondsToSeconds= dateDifference*0.001; //converts miliseconds to seconds
+//     let duration = milisecondsToSeconds/secondsInDay;
+//     let less:string='less than a year ago'
+//     if (duration>=1){
+//       return duration;
+//     }else{
+//       return less;
+//     }
+
+
+//   }
+
+// }
+  if (value) {
+    const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+    if (seconds < 29)
+    return 'Just now';
+    const intervals = {
+      'year': 31536000,
+      'month': 2592000,
+      'week': 604800,
+      'day': 86400,
+      'hour': 3600,
+      'minute': 60,
+      'second': 1
+    };
+    let counter;
+    for (const i in intervals) {
+      counter = Math.floor(seconds / intervals[i]);
+      if (counter > 0)
+      if (counter === 1) {
+        return counter + ' ' + i + ' ago';
+      } else {
+        return counter + ' ' + i + 's ago';
+      }
     }
-
-
   }
+  return value;
+}
 
 }
