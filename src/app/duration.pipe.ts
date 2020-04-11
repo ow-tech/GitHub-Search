3d829ef53dc697c5dc30fde19fe4435643602d82
ow-tech <wagwanwekon@outloo.com>
2020-04-11 14:20:41 +0300

    duration pipe logic

--- src/app/duration.pipe.ts
+++ src/app/duration.pipe.ts
@@ -6,14 +6,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DurationPipe implements PipeTransform {
 
   transform(value: any,): any {
-    console.log(value)
-    let currentDate:any=new Date();
-    let today:any=new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate())
-    // new Date(today.getFullYear(), today.getMonth(), today.getDate())
-    let createdDate:any=new Date(value).getFullYear().getMonth().getDate();
-    let duration= currentDate-createdDate;
-
-    return duration;
+    
+    let currentDate:any=new Date().getFullYear();
+    let newValue= value.getFullYear();
+    // let today:any=new Date().getFullYear();
+    let duration =(currentDate - newValue)
+    console.log(duration)
+    if(duration>=1){
+      return duration;
+    }else{
+      return 'less than a year'
+    }
   }
 
 }
